82fbc360a3213b4134d239817d46f58e094e124a
Misbah Ansori <misbah.ansori24@gmail.com>
2025-03-27 18:21:56 +0700

    fix

--- app/registry/components/ui/stepper/StepperIndicator.vue
+++ app/registry/components/ui/stepper/StepperIndicator.vue
@@ -23,7 +23,7 @@ const forwarded = useForwardProps(delegatedProps);
     v-bind="forwarded"
     :class="
       cn(
-        'bg-muted text-muted-foreground data-[state=active]:bg-primary data-[state=completed]:bg-primary data-[state=active]:text-primary-foreground data-[state=completed]:text-primary-foreground relative flex size-6 shrink-0 items-center justify-center rounded-full text-xs font-medium',
+        'bg-muted text-muted-foreground group-data-[state=active]/step:bg-primary group-data-[state=completed]/step:bg-primary group-data-[state=active]/step:text-primary-foreground group-data-[state=completed]/step:text-primary-foreground relative flex size-6 shrink-0 items-center justify-center rounded-full text-xs font-medium',
         props.class,
       )
     "
