909fdaa1ede673e3330f8f4186c44951f70b042f
Misbah Ansori <misbah.ansori24@gmail.com>
2025-03-27 08:31:02 +0700

    Fix : update alert dialog ui

--- app/registry/components/ui/alert-dialog/AlertDialogFooter.vue
+++ app/registry/components/ui/alert-dialog/AlertDialogFooter.vue
@@ -10,10 +10,7 @@ const props = defineProps<{
 <template>
   <div
     :class="
-      cn(
-        'flex flex-col-reverse sm:flex-row sm:justify-end sm:gap-x-2',
-        props.class,
-      )
+      cn('flex flex-col-reverse gap-3 sm:flex-row sm:justify-end', props.class)
     "
   >
     <slot />
