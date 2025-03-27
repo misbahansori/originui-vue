a9365b521eb44274bf6c73fc2bc8e986c9e390f4
Misbah Ansori <misbah.ansori24@gmail.com>
2025-03-27 13:06:54 +0700

    refactor: update theme handling in comp-150 to use color mode for improved accessibility

--- app/registry/components/comp-150.vue
+++ app/registry/components/comp-150.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import { ref, useId } from "vue";
-
 const id = useId();
-const theme = ref("light");
+const colorMode = useColorMode();
 
-const toggleTheme = () => {
-  theme.value = theme.value === "dark" ? "light" : "dark";
+const toggleColorMode = () => {
+  colorMode.preference = colorMode.preference === "dark" ? "light" : "dark";
 };
 </script>
 
@@ -20,20 +18,19 @@ const toggleTheme = () => {
         :name="id"
         :id="id"
         class="peer sr-only"
-        :checked="theme === 'dark'"
-        @change="toggleTheme"
+        :checked="colorMode.preference === 'dark'"
+        @change="toggleColorMode"
       />
       <label
         class="group border-input bg-background text-foreground hover:bg-accent hover:text-accent-foreground peer-focus-visible:border-ring peer-focus-visible:ring-ring/50 relative inline-flex size-9 items-center justify-center rounded-md border shadow-xs transition-[color,box-shadow] outline-none peer-focus-visible:ring-[3px]"
         :for="id"
-        :aria-label="`Switch to ${theme === 'dark' ? 'light' : 'dark'} mode`"
+        :aria-label="`Switch to ${colorMode.preference === 'dark' ? 'light' : 'dark'} mode`"
       >
-        <!-- Note: After dark mode implementation, rely on dark: prefix rather than group-peer-checked: -->
         <Icon
           name="lucide:moon"
           size="16"
           class="shrink-0 scale-0 opacity-0 transition-all"
-          :class="{ 'scale-100 opacity-100': theme === 'dark' }"
+          :class="{ 'scale-100 opacity-100': colorMode.preference === 'dark' }"
           aria-hidden="true"
         />
         <Icon
@@ -41,8 +38,8 @@ const toggleTheme = () => {
           size="16"
           class="absolute shrink-0 transition-all"
           :class="{
-            'scale-100 opacity-100': theme === 'light',
-            'scale-0 opacity-0': theme === 'dark',
+            'scale-100 opacity-100': colorMode.preference === 'light',
+            'scale-0 opacity-0': colorMode.preference === 'dark',
           }"
           aria-hidden="true"
         />
