e23ac05cb31f6d65f68d15317ce47de9af4ea29d
Misbah Ansori <misbah.ansori24@gmail.com>
2025-04-04 16:33:57 +0700

    fix: comp-405

--- app/registry/components/comp-405.vue
+++ app/registry/components/comp-405.vue
@@ -1,1 +1,28 @@
-<template></template>
+<template>
+  <div class="flex -space-x-3">
+    <img
+      class="ring-background rounded-full ring-2"
+      src="/avatar-80-03.jpg"
+      width="40"
+      height="40"
+    />
+    <img
+      class="ring-background rounded-full ring-2"
+      src="/avatar-80-04.jpg"
+      width="40"
+      height="40"
+    />
+    <img
+      class="ring-background rounded-full ring-2"
+      src="/avatar-80-05.jpg"
+      width="40"
+      height="40"
+    />
+    <img
+      class="ring-background rounded-full ring-2"
+      src="/avatar-80-06.jpg"
+      width="40"
+      height="40"
+    />
+  </div>
+</template>
