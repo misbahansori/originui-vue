906089356ce7db1c44cadab2f2221472e40ef3fb
Misbah Ansori <misbah.ansori24@gmail.com>
2025-04-05 19:26:38 +0700

    fix: imports

--- app/registry/default/components/comp-05.vue
+++ app/registry/default/components/comp-05.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { Input } from "@/registry/default/ui/input";
+import { Label } from "@/registry/default/ui/label";
+import { useId } from "vue";
+
 const id = useId();
 </script>
 
