e2739028e06e2dd1085888fe1b1ea5a42d5c41ac
Misbah Ansori <misbah.ansori24@gmail.com>
2025-04-06 06:16:03 +0700

    refactor: update imports

--- app/registry/default/components/comp-115.vue
+++ app/registry/default/components/comp-115.vue
@@ -1,15 +1,15 @@
+<script setup lang="ts">
+import { Button } from "@/registry/default/ui/button";
+import { LucideChevronLeft } from "lucide-vue-next";
+</script>
+
 <template>
   <Button class="relative ps-12">
     Previous
     <span
       class="bg-primary-foreground/15 pointer-events-none absolute inset-y-0 start-0 flex w-9 items-center justify-center"
     >
-      <Icon
-        name="lucide:chevron-left"
-        class="opacity-60"
-        size="16"
-        aria-hidden="true"
-      />
+      <LucideChevronLeft class="opacity-60" :size="16" aria-hidden="true" />
     </span>
   </Button>
 </template>
--- app/registry/default/components/comp-117.vue
+++ app/registry/default/components/comp-117.vue
@@ -1,11 +1,11 @@
+<script setup lang="ts">
+import { Button } from "@/registry/default/ui/button";
+import { LucideThumbsUp } from "lucide-vue-next";
+</script>
+
 <template>
   <Button class="py-0 pe-0" variant="outline">
-    <Icon
-      name="lucide:thumbs-up"
-      class="opacity-60"
-      size="16"
-      aria-hidden="true"
-    />
+    <LucideThumbsUp class="opacity-60" :size="16" aria-hidden="true" />
     Like
     <span
       class="text-muted-foreground before:bg-input relative ms-1 inline-flex h-full items-center justify-center rounded-full px-3 text-xs font-medium before:absolute before:inset-0 before:left-0 before:w-px"
