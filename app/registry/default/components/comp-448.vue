06355bbcd1eb1aaa713c3ea0e83a7f64079cf540
Misbah Ansori <misbah.ansori24@gmail.com>
2025-04-06 19:03:00 +0700

    refactor: update imports

--- app/registry/default/components/comp-448.vue
+++ app/registry/default/components/comp-448.vue
@@ -1,9 +1,21 @@
+<script setup lang="ts">
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/registry/default/ui/breadcrumb";
+import { LucideHome } from "lucide-vue-next";
+</script>
+
 <template>
   <Breadcrumb>
     <BreadcrumbList>
       <BreadcrumbItem>
         <BreadcrumbLink href="#">
-          <Icon name="lucide:home" class="size-4" aria-hidden="true" />
+          <LucideHome class="size-4" aria-hidden="true" />
           <span class="sr-only">Home</span>
         </BreadcrumbLink>
       </BreadcrumbItem>
--- app/registry/default/components/comp-449.vue
+++ app/registry/default/components/comp-449.vue
@@ -1,16 +1,28 @@
+<script setup lang="ts">
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/registry/default/ui/breadcrumb";
+import { LucideComponent, LucideHome } from "lucide-vue-next";
+</script>
+
 <template>
   <Breadcrumb>
     <BreadcrumbList>
       <BreadcrumbItem>
         <BreadcrumbLink href="#" class="inline-flex items-center gap-1.5">
-          <Icon name="lucide:home" class="size-4" aria-hidden="true" />
+          <LucideHome class="size-4" aria-hidden="true" />
           Home
         </BreadcrumbLink>
       </BreadcrumbItem>
       <BreadcrumbSeparator />
       <BreadcrumbItem>
         <BreadcrumbLink href="#" class="inline-flex items-center gap-1.5">
-          <Icon name="lucide:component" class="size-4" aria-hidden="true" />
+          <LucideComponent class="size-4" aria-hidden="true" />
           Components
         </BreadcrumbLink>
       </BreadcrumbItem>
