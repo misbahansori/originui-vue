cedbcdf8571390840a1dea64cb2e12e84cb98218
Misbah Ansori <misbah.ansori24@gmail.com>
2025-04-07 09:02:16 +0700

    fix: comp-40

--- app/registry/default/components/comp-40.vue
+++ app/registry/default/components/comp-40.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { Label } from "@/registry/default/ui/label";
-import { LucideClock } from "lucide-vue-next";
-import { ref, useId } from "vue";
+import { DateFieldInput, DateFieldRoot } from "reka-ui";
+import { useId } from "vue";
 
 const id = useId();
-const dateTime = ref(new Date());
 </script>
 
 <template>
@@ -12,29 +11,44 @@ const dateTime = ref(new Date());
     <Label :for="id" class="text-foreground text-sm font-medium">
       Date and time input
     </Label>
-    <div class="relative">
-      <div
-        class="border-input bg-background text-foreground focus-within:border-ring focus-within:ring-ring/50 flex h-9 items-center rounded-md border px-3 shadow-xs transition-[color,box-shadow] outline-none focus-within:ring-[3px]"
-      >
-        <input
-          :id="id"
-          v-model="dateTime"
-          type="datetime-local"
-          class="w-full bg-transparent pe-6 text-sm focus:outline-none"
-        />
-      </div>
-      <div
-        class="text-muted-foreground/80 pointer-events-none absolute inset-y-0 end-0 z-10 flex items-center justify-center pe-3"
-      >
-        <LucideClock :size="16" aria-hidden="true" />
-      </div>
-    </div>
+    <DateFieldRoot
+      :id="id"
+      granularity="minute"
+      :hourCycle="24"
+      v-slot="{ segments }"
+      class="border-input bg-background text-foreground focus-within:border-ring focus-within:ring-ring/50 flex h-9 items-center rounded-md border px-3 shadow-xs transition-[color,box-shadow] outline-none focus-within:ring-[3px]"
+    >
+      <template v-for="item in segments" :key="item.part">
+        <DateFieldInput
+          v-if="item.part === 'literal'"
+          :part="item.part"
+          class="text-muted-foreground/70"
+        >
+          {{ item.value }}
+        </DateFieldInput>
+        <DateFieldInput
+          v-else
+          :part="item.part"
+          class="data-[placeholder]:text-muted-foreground/70 focus:bg-muted rounded p-0.5 text-sm focus:shadow-black focus:outline-none"
+        >
+          {{ item.value }}
+        </DateFieldInput>
+      </template>
+    </DateFieldRoot>
     <p
       class="text-muted-foreground mt-2 text-xs"
       role="region"
       aria-live="polite"
     >
-      Built with native datetime input
+      Built with reka-ui
+      <a
+        class="hover:text-foreground underline"
+        href="https://reka-ui.com/docs/components/date-field"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        DateField
+      </a>
     </p>
   </div>
 </template>
