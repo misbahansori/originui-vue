0bfd10d6bbc5f7fea8ede67d9484245029dc7d77
Misbah Ansori <misbah.ansori24@gmail.com>
2025-04-11 07:29:10 +0700

    fix: theme toggle

--- app/components/ThemeToggle.vue
+++ app/components/ThemeToggle.vue
@@ -10,29 +10,17 @@ const toggle = () => {
 
 <template>
   <Button variant="ghost" size="icon" class="size-9" @click="toggle">
-    <ColorScheme>
-      <template #placeholder>
-        <LucideSun
-          :size="16"
-          class="text-muted-foreground hover:text-foreground/80 size-4"
-          aria-hidden="true"
-        />
-      </template>
+    <LucideSun
+      :size="16"
+      class="text-muted-foreground hover:text-foreground/80 size-4 dark:hidden"
+      aria-hidden="true"
+    />
 
-      <LucideSun
-        v-if="colorMode.value === 'light'"
-        :size="16"
-        class="text-muted-foreground hover:text-foreground/80 size-4 dark:hidden"
-        aria-hidden="true"
-      />
-
-      <LucideMoon
-        v-else-if="colorMode.value === 'dark'"
-        :size="16"
-        class="text-muted-foreground hover:text-foreground/80 hidden size-4 dark:block"
-        aria-hidden="true"
-      />
-      <span class="sr-only">Toggle theme</span>
-    </ColorScheme>
+    <LucideMoon
+      :size="16"
+      class="text-muted-foreground hover:text-foreground/80 hidden size-4 dark:block"
+      aria-hidden="true"
+    />
+    <span class="sr-only">Toggle theme</span>
   </Button>
 </template>
