7ff36c3b5d2edd7b4aed9242da95a615d25a3269
Misbah Ansori <misbah.ansori24@gmail.com>
2025-04-19 06:08:25 +0700

    feat: update calendar components

--- app/registry/default/components/comp-487.vue
+++ app/registry/default/components/comp-487.vue
@@ -12,9 +12,22 @@ const value = ref(today(getLocalTimeZone())) as Ref<DateValue>;
 </script>
 
 <template>
-  <Calendar
-    v-model="value"
-    :weekday-format="'short'"
-    class="rounded-md border"
-  />
+  <div>
+    <Calendar
+      v-model="value"
+      :weekday-format="'short'"
+      class="rounded-md border"
+    />
+    <p class="text-muted-foreground mt-4 text-center text-xs">
+      Calendar -
+      <a
+        class="hover:text-foreground underline"
+        href="https://reka-ui.com/docs/components/calendar#calendar"
+        target="_blank"
+        rel="noopener nofollow"
+      >
+        Reka UI
+      </a>
+    </p>
+  </div>
 </template>
--- app/registry/default/components/comp-490.vue
+++ app/registry/default/components/comp-490.vue
@@ -1,1 +1,28 @@
-<template></template>
+<script setup lang="ts">
+import { Calendar } from "@/registry/default/ui/calendar";
+import {
+  getLocalTimeZone,
+  today,
+  type DateValue,
+} from "@internationalized/date";
+import { ref } from "vue";
+
+const value = ref(today(getLocalTimeZone())) as Ref<DateValue>;
+</script>
+
+<template>
+  <div>
+    <Calendar v-model="value" class="rounded-md border p-2" />
+    <p class="text-muted-foreground mt-4 text-center text-xs">
+      Calendar -
+      <a
+        class="hover:text-foreground underline"
+        href="https://reka-ui.com/docs/components/calendar#calendar"
+        target="_blank"
+        rel="noopener nofollow"
+      >
+        Reka UI
+      </a>
+    </p>
+  </div>
+</template>
