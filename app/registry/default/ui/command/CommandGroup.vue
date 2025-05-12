16f816b9c497cd6aa46eff0d8d90a8ccd3701886
Misbah Ansori <misbah.ansori24@gmail.com>
2025-05-12 11:10:14 +0700

    fix: comand item

--- app/registry/default/ui/command/CommandGroup.vue
+++ app/registry/default/ui/command/CommandGroup.vue
@@ -39,7 +39,12 @@ onUnmounted(() => {
     v-bind="delegatedProps"
     :id="id"
     data-slot="command-group"
-    :class="cn('text-foreground overflow-hidden p-1', props.class)"
+    :class="
+      cn(
+        'text-foreground [&_[cmdk-group-heading]]:text-muted-foreground overflow-hidden p-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium',
+        props.class,
+      )
+    "
     :hidden="isRender ? undefined : true"
   >
     <ListboxGroupLabel
