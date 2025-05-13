4d9d2c83f43601e37483275c013c2447504585a2
Misbah Ansori <misbah.ansori24@gmail.com>
2025-05-13 16:57:55 +0700

    feat: add extended-07 and extended-08 components to registry and update extended-06 layout

--- app/registry/default/components/extended-08.vue
+++ app/registry/default/components/extended-08.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
 import { Button } from "@/registry/default/ui/button";
-import { LucideFile, LucidePlus, LucideRefreshCcw } from "lucide-vue-next";
+import {
+  LucideCalendar,
+  LucideFile,
+  LucidePlus,
+  LucideRefreshCcw,
+  LucideUser,
+} from "lucide-vue-next";
 </script>
 
 <template>
   <div class="flex items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
     <div class="flex w-full max-w-md flex-col items-center gap-6 text-center">
-      <div class="bg-muted flex items-center justify-center rounded-full p-4">
-        <LucideFile class="size-6" />
+      <div class="flex items-center gap-3">
+        <div class="-rotate-8 rounded-md border p-3 shadow">
+          <LucideFile class="text-muted-foreground size-5" />
+        </div>
+        <div class="-translate-y-2 rounded-md border p-3 shadow">
+          <LucideCalendar class="text-muted-foreground size-5" />
+        </div>
+        <div class="rotate-8 rounded-md border p-3 shadow">
+          <LucideUser class="text-muted-foreground size-5" />
+        </div>
       </div>
       <div class="flex flex-col gap-2">
         <h1 class="text-lg font-bold">No data found</h1>
