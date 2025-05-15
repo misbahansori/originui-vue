2e1717639a2fb078880f469f4e3e9757803eac73
Misbah Ansori <misbah.ansori24@gmail.com>
2025-05-15 20:51:08 +0700

    wip

--- app/registry/default/components/extended-13.vue
+++ app/registry/default/components/extended-13.vue
@@ -1,65 +1,71 @@
 <script setup lang="ts">
+import { cn } from "@/lib/utils";
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/registry/default/ui/card";
+
+const cards = [
+  {
+    title: "Smart Analytics Dashboard",
+    description:
+      "Get real-time insights with customizable widgets and interactive data visualization tools.",
+    class: "col-span-1 lg:col-span-2",
+    img: "/img/long-jobs.webp",
+  },
+  {
+    title: "Cross-Platform Integration",
+    description:
+      "Seamlessly connect with your favorite tools and services through our extensive API ecosystem.",
+    class: "col-span-1 lg:col-span-2",
+    img: "/img/multi-users.webp",
+  },
+  {
+    title: "Advanced Security Suite",
+    description:
+      "Enterprise-grade security with end-to-end encryption and multi-factor authentication.",
+    class: "col-span-1 lg:col-span-2",
+    img: "/img/tasks-scheduler.webp",
+  },
+  {
+    title: "Automated Workflow Builder",
+    description:
+      "Create custom workflows with drag-and-drop interface and no-code automation tools.",
+    class: "col-span-1 lg:col-span-3",
+    img: "/img/scenarios.webp",
+  },
+  {
+    title: "AI-Powered Insights",
+    description:
+      "Leverage machine learning to predict trends and provide actionable business recommendations.",
+    class: "col-span-1 lg:col-span-3",
+    img: "/img/explore.webp",
+  },
+];
 </script>
 
 <template>
   <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-6">
-    <Card class="col-span-1 gap-2 p-2 lg:col-span-2">
-      <div class="bg-muted h-56 w-full rounded-md border"></div>
+    <Card
+      v-for="card in cards"
+      :key="card.title"
+      :class="cn('gap-2 p-2', card.class)"
+    >
+      <div
+        class="flex h-56 w-full items-center justify-center overflow-hidden rounded-md border bg-stone-900"
+      >
+        <img
+          :src="card.img"
+          alt="card image"
+          class="h-auto w-auto object-cover"
+        />
+      </div>
       <CardHeader class="p-2">
-        <CardTitle>Smart Analytics Dashboard</CardTitle>
-        <CardDescription>
-          Get real-time insights with customizable widgets and interactive data
-          visualization tools.
-        </CardDescription>
-      </CardHeader>
-    </Card>
-
-    <Card class="col-span-1 gap-2 p-2 lg:col-span-2">
-      <div class="bg-muted h-56 w-full rounded-md border"></div>
-      <CardHeader class="p-2">
-        <CardTitle>Cross-Platform Integration</CardTitle>
-        <CardDescription>
-          Seamlessly connect with your favorite tools and services through our
-          extensive API ecosystem.
-        </CardDescription>
-      </CardHeader>
-    </Card>
-
-    <Card class="col-span-1 gap-2 p-2 lg:col-span-2">
-      <div class="bg-muted h-56 w-full rounded-md border"></div>
-      <CardHeader class="p-2">
-        <CardTitle>Advanced Security Suite</CardTitle>
-        <CardDescription>
-          Enterprise-grade security with end-to-end encryption and multi-factor
-          authentication.
-        </CardDescription>
-      </CardHeader>
-    </Card>
-
-    <Card class="col-span-1 gap-2 p-2 lg:col-span-3">
-      <div class="bg-muted h-56 w-full rounded-md border"></div>
-      <CardHeader class="p-2">
-        <CardTitle>Automated Workflow Builder</CardTitle>
-        <CardDescription>
-          Create custom workflows with drag-and-drop interface and no-code
-          automation tools.
-        </CardDescription>
-      </CardHeader>
-    </Card>
-
-    <Card class="col-span-1 gap-2 p-2 lg:col-span-3">
-      <div class="bg-muted h-56 w-full rounded-md border"></div>
-      <CardHeader class="p-2">
-        <CardTitle>AI-Powered Insights</CardTitle>
-        <CardDescription>
-          Leverage machine learning to predict trends and provide actionable
-          business recommendations.
+        <CardTitle class="text-lg font-semibold">{{ card.title }}</CardTitle>
+        <CardDescription class="text-sm/relaxed">
+          {{ card.description }}
         </CardDescription>
       </CardHeader>
     </Card>
