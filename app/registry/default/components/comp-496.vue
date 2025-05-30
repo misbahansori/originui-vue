c970c343bb3ab46119800728caae98b602e669dc
Misbah Ansori <misbah.ansori24@gmail.com>
2025-05-31 06:52:27 +0700

    feat: add comp 496

--- app/registry/default/components/comp-496.vue
+++ app/registry/default/components/comp-496.vue
@@ -1,7 +1,88 @@
+<script lang="ts" setup>
+import { cn } from "@/lib/utils";
+import {
+  CalendarCell,
+  CalendarCellTrigger,
+  CalendarGrid,
+  CalendarGridBody,
+  CalendarGridHead,
+  CalendarGridRow,
+  CalendarHeadCell,
+  CalendarHeader,
+  CalendarHeading,
+} from "@/registry/default/ui/calendar";
+import {
+  getLocalTimeZone,
+  today,
+  type DateValue,
+} from "@internationalized/date";
+import { LucideChevronLeft, LucideChevronRight } from "lucide-vue-next";
+import { CalendarNext, CalendarPrev, CalendarRoot } from "reka-ui";
+import { buttonVariants } from "~/registry/default/ui/button";
+
+const value = ref(today(getLocalTimeZone())) as Ref<DateValue>;
+</script>
+
 <template>
-  <div class="flex items-center justify-center py-12">
-    <span class="text-muted-foreground text-sm italic">
-      Component not yet implemented.
-    </span>
+  <div>
+    <CalendarRoot
+      v-model="value"
+      v-slot="{ grid, weekDays }"
+      data-slot="calendar"
+      class="rounded-md border p-3"
+    >
+      <CalendarHeader class="flex justify-between">
+        <CalendarHeading class="flex-1 pl-2" />
+        <div class="flex items-center gap-1">
+          <CalendarPrev
+            :class="cn(buttonVariants({ variant: 'ghost', size: 'icon' }))"
+          >
+            <LucideChevronLeft class="size-4" />
+          </CalendarPrev>
+          <CalendarNext
+            :class="cn(buttonVariants({ variant: 'ghost', size: 'icon' }))"
+          >
+            <LucideChevronRight class="size-4" />
+          </CalendarNext>
+        </div>
+      </CalendarHeader>
+      <div class="mt-4 flex flex-col gap-y-4 sm:flex-row sm:gap-x-4 sm:gap-y-0">
+        <CalendarGrid v-for="month in grid" :key="month.value.toString()">
+          <CalendarGridHead>
+            <CalendarGridRow>
+              <CalendarHeadCell v-for="day in weekDays" :key="day">
+                {{ day }}
+              </CalendarHeadCell>
+            </CalendarGridRow>
+          </CalendarGridHead>
+          <CalendarGridBody>
+            <CalendarGridRow
+              v-for="(weekDates, index) in month.rows"
+              :key="`weekDate-${index}`"
+              class="mt-2 w-full"
+            >
+              <CalendarCell
+                v-for="weekDate in weekDates"
+                :key="weekDate.toString()"
+                :date="weekDate"
+              >
+                <CalendarCellTrigger :day="weekDate" :month="month.value" />
+              </CalendarCell>
+            </CalendarGridRow>
+          </CalendarGridBody>
+        </CalendarGrid>
+      </div>
+    </CalendarRoot>
+    <p class="text-muted-foreground mt-4 text-center text-xs">
+      Right navigation -
+      <a
+        class="hover:text-foreground underline"
+        href="https://reka-ui.com/docs/components/calendar#calendar"
+        target="_blank"
+        rel="noopener nofollow"
+      >
+        Reka UI Calendar
+      </a>
+    </p>
   </div>
 </template>
