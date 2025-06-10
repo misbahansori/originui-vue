750ba61f0a991ddbee6198d6f87ffc174d9a9cb9
peoray <peoray@yahoo.com>
2025-06-10 19:55:53 +0100

    feat: add easings page with SEO meta tags and define Link interface and update links in Header component

---
+++ app/pages/easings.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// SEO meta tags
+useSeoMeta({
+  title: "Tailwind CSS Easing Classes - Origin UI Vue",
+  description:
+    "A collection of easing utility classes to enhance your Tailwind CSS animations and transitions.",
+});
+</script>
+
+<template>
+  <div>
+    <PageHeader title="Tailwind CSS easing classes">
+      A set of easing functions ready to copy and paste into your Tailwind CSS
+      project.
+    </PageHeader>
+  </div>
+</template>
