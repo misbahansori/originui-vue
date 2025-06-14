ae7b83a493be3ab5bbcaeb1620f6ebec369107ec
Misbah Ansori <misbah.ansori24@gmail.com>
2025-06-14 11:28:14 +0700

    wip

--- app/registry/default/components/comp-565.vue
+++ app/registry/default/components/comp-565.vue
@@ -48,15 +48,31 @@ const items: Item[] = [
 </script>
 
 <template>
-  <div class="flex h-full flex-col gap-2">
+  <div>
     <Tree
       :items="items"
       :getKey="(item) => item.name"
       v-slot="{ flattenItems }"
+      :defaultExpanded="['Company', 'Engineering', 'Frontend', 'Design System']"
     >
       <TreeItem v-for="item in flattenItems" v-bind="item">
         {{ item.value.name }}
       </TreeItem>
     </Tree>
+    <p
+      aria-live="polite"
+      role="region"
+      className="text-muted-foreground mt-2 text-xs"
+    >
+      Basic tree with no extra features ∙
+      <a
+        href="https://reka-ui.com/docs/components/tree"
+        class="hover:text-foreground underline"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Reka UI Tree
+      </a>
+    </p>
   </div>
 </template>
