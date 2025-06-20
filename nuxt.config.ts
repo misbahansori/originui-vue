87a2375ed09475896aebb9a7cf2dfaf88d3cabd3
Misbah Ansori <misbah.ansori24@gmail.com>
2025-06-20 07:10:29 +0700

    feat: add prerender for additional routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,8 +81,8 @@ export default defineNuxtConfig({
   },
 
   routeRules: {
-    "/": {
-      prerender: true,
-    },
+    "/": { prerender: true },
+    "/easings": { prerender: true },
+    "/layouts": { prerender: true },
   },
 });
