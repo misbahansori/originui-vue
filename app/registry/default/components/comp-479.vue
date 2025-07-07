4761e2e2c7d3235461c8f8dea93746780c972e07
Misbah Ansori <misbah.ansori24@gmail.com>
2025-07-07 07:47:12 +0700

    refactor: replace  API URL with local fetch for user data

--- app/registry/default/components/comp-479.vue
+++ app/registry/default/components/comp-479.vue
@@ -108,11 +108,8 @@ const sorting = ref<SortingState>([
 ]);
 
 onMounted(async () => {
-  const res = await fetch(
-    "https://res.cloudinary.com/dlzlfasou/raw/upload/users-01_fertyx.json",
-  );
-  const json = await res.json();
-  data.value = json.slice(0, 5); // Limit to 5 items
+  const res = await $fetch("/api/users");
+  data.value = res.slice(0, 5);
 });
 
 const table = useVueTable({
