65728160e731af7371232d8244888ced4069aba3
Misbah Ansori <misbah.ansori24@gmail.com>
2025-08-24 10:51:40 +0700

    chore: update avatar component

--- app/registry/default/ui/avatar/AvatarImage.vue
+++ app/registry/default/ui/avatar/AvatarImage.vue
@@ -9,7 +9,7 @@ const props = defineProps<AvatarImageProps>();
   <AvatarImage
     data-slot="avatar-image"
     v-bind="props"
-    class="h-full w-full object-cover"
+    class="aspect-square size-full"
   >
     <slot />
   </AvatarImage>
