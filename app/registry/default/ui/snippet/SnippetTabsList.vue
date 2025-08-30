2ce6da760d64dec4eb817bc48b7864840751e12f
Misbah Ansori <misbah.ansori24@gmail.com>
2025-08-30 10:18:08 +0700

    refactor: replace computed with reactiveOmit

--- app/registry/default/ui/snippet/SnippetTabsList.vue
+++ app/registry/default/ui/snippet/SnippetTabsList.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
 import { cn } from "@/lib/utils";
+import { reactiveOmit } from "@vueuse/core";
 import { TabsList, type TabsListProps } from "reka-ui";
-import { computed, type HTMLAttributes } from "vue";
+import type { HTMLAttributes } from "vue";
 
 const props = defineProps<
   TabsListProps & { class?: HTMLAttributes["class"] }
 >();
 
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props;
-  return delegated;
-});
+const delegatedProps = reactiveOmit(props, "class");
 </script>
 
 <template>
--- app/registry/default/ui/snippet/SnippetTabsTrigger.vue
+++ app/registry/default/ui/snippet/SnippetTabsTrigger.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 import { cn } from "@/lib/utils";
+import { reactiveOmit } from "@vueuse/core";
 import { TabsTrigger, type TabsTriggerProps, useForwardProps } from "reka-ui";
-import { computed, type HTMLAttributes } from "vue";
+import type { HTMLAttributes } from "vue";
 
 const props = defineProps<
   TabsTriggerProps & { class?: HTMLAttributes["class"] }
 >();
 
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props;
-  return delegated;
-});
-
+const delegatedProps = reactiveOmit(props, "class");
 const forwardedProps = useForwardProps(delegatedProps);
 </script>
 
