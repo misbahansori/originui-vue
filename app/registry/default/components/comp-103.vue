852584b37f3a84dc9dd8a2afe2fc854fad70217e
Misbah Ansori <misbah.ansori24@gmail.com>
2025-10-09 05:21:29 +0700

    refactor: integrate ButtonGroup into comp-103

--- app/registry/default/components/comp-103.vue
+++ app/registry/default/components/comp-103.vue
@@ -1,30 +1,34 @@
 <script setup lang="ts">
 import { Button } from "@/registry/default/ui/button";
+import {
+  ButtonGroup,
+  ButtonGroupText,
+} from "@/registry/default/ui/button-group";
 import { LucideChevronDown, LucideChevronUp } from "lucide-vue-next";
 </script>
 
 <template>
-  <div
-    class="inline-flex -space-x-px rounded-full shadow-xs rtl:space-x-reverse"
-  >
-    <Button
-      class="rounded-none shadow-none first:rounded-s-full last:rounded-e-full focus-visible:z-10"
-      size="icon"
-      aria-label="Upvote"
-    >
-      <LucideChevronUp :size="16" aria-hidden="true" />
-    </Button>
-    <span
-      class="bg-primary text-primary-foreground flex items-center px-1 text-sm font-medium"
-    >
-      235
-    </span>
-    <Button
-      class="rounded-none shadow-none first:rounded-s-full last:rounded-e-full focus-visible:z-10"
-      size="icon"
-      aria-label="Downvote"
-    >
-      <LucideChevronDown :size="16" aria-hidden="true" />
-    </Button>
+  <div class="flex items-center justify-center">
+    <ButtonGroup>
+      <Button
+        class="rounded-none shadow-none first:rounded-s-full last:rounded-e-full focus-visible:z-10"
+        size="icon"
+        aria-label="Upvote"
+      >
+        <LucideChevronUp :size="16" aria-hidden="true" />
+      </Button>
+      <ButtonGroupText
+        class="bg-primary text-primary-foreground border-transparent px-1 text-sm font-medium"
+      >
+        235
+      </ButtonGroupText>
+      <Button
+        class="rounded-none shadow-none first:rounded-s-full last:rounded-e-full focus-visible:z-10"
+        size="icon"
+        aria-label="Downvote"
+      >
+        <LucideChevronDown :size="16" aria-hidden="true" />
+      </Button>
+    </ButtonGroup>
   </div>
 </template>
