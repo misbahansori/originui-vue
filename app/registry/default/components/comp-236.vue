7c4e33139d07e14599529fd0f8eae4769fffbfe6
Misbah Ansori <misbah.ansori24@gmail.com>
2026-02-17 12:18:41 +0700

    chore: build registry

--- app/registry/default/components/comp-236.vue
+++ app/registry/default/components/comp-236.vue
@@ -9,8 +9,15 @@ const id = useId();
 <template>
   <div class="*:not-first:mt-2">
     <Label :for="id">Multiple select (native)</Label>
-    <div class="border-input overflow-hidden rounded-md border">
-      <NativeSelect :id="id" :modelValue="['1', '5']" multiple class="rounded-none border-none">
+    <div
+      class="border-input focus-within:border-ring focus-within:ring-ring/50 overflow-hidden rounded-md border focus-within:ring-[3px]"
+    >
+      <NativeSelect
+        :id="id"
+        :modelValue="['1', '5']"
+        multiple
+        class="[&_option:checked]:bg-accent h-auto rounded-none border-none px-0 py-0.5 focus-visible:ring-0 [&>option]:px-3 [&>option]:py-1"
+      >
         <option value="1">React</option>
         <option value="2">Next.js</option>
         <option value="3">Astro</option>
